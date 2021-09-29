f5257d6023b2c06f35f03eaee99b0757095ec1fc
emmanuellekamwa <61285247+emmanuellekamwa@users.noreply.github.com>
2021-09-29 15:39:42 +0100

    Fix linter errors

--- example_school_library_decorator/person.rb
+++ example_school_library_decorator/person.rb
@@ -4,8 +4,7 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', is_parent_permitted: true)
     @id = Random.rand(1..1000)
     @corrector = Corrector.new
     @age = age
