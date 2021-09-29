2604febc698882930e489ade3cb99471a7e49fa9
emmanuellekamwa <61285247+emmanuellekamwa@users.noreply.github.com>
2021-09-29 13:41:58 +0100

    Fix linter errors

--- example_school_library_decorator/functions.rb
+++ example_school_library_decorator/functions.rb
@@ -1,30 +1,32 @@
-def list_books(books, userSelect)
-    book_list = ["Rapunzel", "Oliver Twist", "Othello", "Romeo and Juliette"]
-    people = ["John", "Isaac", "Emma", "Victor"]
-    until !userSelect do
-    if books == 1
-        puts "The following is a list of books"
-        puts book_list
-        break
-    elsif books == 2
-        puts "The following is a list of people"
-        puts people
-        break
-    elsif books == 3
-        puts "You have selected " + books.to_s
-        break
-    elsif books == 4
-        puts "You have selected " + books.to_s
-        break
-    elsif books == 5
-        puts "You have selected " + books.to_s
-        break
-    elsif books == 6
-        puts "You have selected " + books.to_s
-        break
-    elsif books == 7
-        puts "Bye. See you next time "
-        break
+def
+  list_books(books, userSelect)
+  book_list = ['Rapunzel', 'Oliver Twist', 'Othello', 'Romeo and Juliette']
+  people = %w[John Isaac Emma Victor]
+  while userSelect
+    case books
+    when 1
+      puts 'The following is a list of books'
+      puts book_list
+      break
+    when 2
+      puts 'The following is a list of people'
+      puts people
+      break
+    when 3
+      puts "You have selected #{books}"
+      break
+    when 4
+      puts "You have selected #{books}"
+      break
+    when 5
+      puts "You have selected #{books}"
+      break
+    when 6
+      puts "You have selected #{books}"
+      break
+    when 7
+      puts 'Bye. See you next time '
+      break
     end
-    end
+  end
 end
