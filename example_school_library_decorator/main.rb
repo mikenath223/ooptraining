e1413a7cbe44ce09c7e870512cd31c84d526274f
emmanuellekamwa <61285247+emmanuellekamwa@users.noreply.github.com>
2021-09-30 19:16:13 +0100

    Make the code interactive

--- example_school_library_decorator/main.rb
+++ example_school_library_decorator/main.rb
@@ -1,20 +1,34 @@
 require_relative 'functions'
 
+functions = Functions.new
+
 puts 'Welcome to School Library App!'
 
-one = '1 - List all books'
-two = '2 - List all people'
-three = '3 - Create a person'
-four = '4 - Create a book'
-five = '5 - Create rental'
-six = '6 - List all rentals for a given person id'
-seven = '7 - Exit'
-puts '============================================'
-puts 'Please choose an option by entering a number: '
-puts '============================================'
-puts "#{one}\n#{two}\n#{three}\n#{four}\n#{five}\n#{six}\n#{seven}\n "
-option = Integer(gets)
-options = [1, 2, 3, 4, 5, 6, 7]
-user_select = options.include?(option)
-list_books(option, user_select)
-puts 'Invalid option. Try again later' unless user_select
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+
+    response = gets.chomp
+
+    case response
+    when '1'
+      functions.list_of_books
+    when '2'
+      functions.list_of_people
+    when '3'
+      functions.create_person
+    when '4'
+      functions.create_book
+    when '5'
+      functions.create_rental
+    when '6'
+      functions.list_of_rentals_for_person_id
+    when '7'
+      puts 'Thank you for using this app!'
+    end
+    puts "\n"
--- example_school_library_decorator/person.rb
+++ example_school_library_decorator/person.rb
@@ -4,12 +4,12 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', is_parent_permitted: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @corrector = Corrector.new
     @age = age
     @name = name
-    @parent_permission = is_parent_permitted
+    @parent_permission = parent_permission
   end
 
   def can_use_services?
