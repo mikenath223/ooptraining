d370015a4445a58a18288342900608422c792c23
emmanuellekamwa <61285247+emmanuellekamwa@users.noreply.github.com>
2021-09-30 19:17:52 +0100

    Fix rubocop errors

--- example_school_library_decorator/main.rb
+++ example_school_library_decorator/main.rb
@@ -4,31 +4,31 @@ functions = Functions.new
 
 puts 'Welcome to School Library App!'
 
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
+puts 'Please choose an option by entering a number:'
+puts '1 - List all books'
+puts '2 - List all people'
+puts '3 - Create a person'
+puts '4 - Create a book'
+puts '5 - Create a rental'
+puts '6 - List all rentals for a given person id'
+puts '7 - Exit'
 
-    response = gets.chomp
+response = gets.chomp
 
-    case response
-    when '1'
-      functions.list_of_books
-    when '2'
-      functions.list_of_people
-    when '3'
-      functions.create_person
-    when '4'
-      functions.create_book
-    when '5'
-      functions.create_rental
-    when '6'
-      functions.list_of_rentals_for_person_id
-    when '7'
-      puts 'Thank you for using this app!'
-    end
-    puts "\n"
+case response
+when '1'
+  functions.list_of_books
+when '2'
+  functions.list_of_people
+when '3'
+  functions.create_person
+when '4'
+  functions.create_book
+when '5'
+  functions.create_rental
+when '6'
+  functions.list_of_rentals_for_person_id
+when '7'
+  puts 'Thank you for using this app!'
+end
+puts "\n"
